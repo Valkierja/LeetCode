b7072568072b0360c529203ab26a3b7a6a2e51fb
KSroido <ksxmyqj@gmail.com>
2022-04-02 20:39:49 +0800

    105

---
+++ 105.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include<algorithm>
+
+class Solution {
+    
+public:
+   
+        int result = INT_MIN;
+        
+        int solve(TreeNode* root){    
+        if ( !root ){ return 0; }
+        int left =  solve(root->left);
+        int right = solve(root->right);
+        int LM = left + root->val;
+        int MR = root->val + right;
+        int LMR = left + right + root->val;
+        int  compare_temp = max(max(LM,MR),max(LMR,root->val)); 
+        result = max( compare_temp, result);
+        return max(max(LM,MR),root->val);
+        }
+ 
+       
+    
+    
+        int maxPathSum(TreeNode* root) {
+            solve(root);
+            return result;
+        
+    }
+};
