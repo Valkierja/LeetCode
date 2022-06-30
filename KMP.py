6f5d31965d22a8e276a19aebec5c3e1230f8eb6c
KSroido <ksxmyqj@gmail.com>
2022-06-30 14:50:11 +0800

    buildNext

---
+++ KMP.py
@@ -0,0 +1,24 @@
+from distutils.util import subst_vars
+
+
+def buildNextArr(subString:str) -> list:
+    next = list(0)
+    maxPrefixLenth = 0
+    i = 1
+    while i < len(subString):
+        if subString[i] == subString[maxPrefixLenth]:
+            
+            maxPrefixLenth += 1
+            next.append(maxPrefixLenth)
+            i+=1
+        else :
+            if maxPrefixLenth == 0:
+                next.append(0)
+                i+=1
+            else:
+                maxPrefixLenth = next[maxPrefixLenth-1]
+                
+    return next
+                
+                
+            
