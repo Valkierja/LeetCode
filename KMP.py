a8fc351f93ab163477f05f7d4884c58b33f20337
KSroido <ksxmyqj@gmail.com>
2022-07-01 12:06:34 +0800

    1

--- KMP.py
+++ KMP.py
@@ -17,6 +17,24 @@ def buildNextArr(subString:str) -> list:
                 maxPrefixLenth = next[maxPrefixLenth-1]
                 
     return next
-                
-                
+def KMP(string:str,subString:str) -> int:
+    next = buildNextArr(subString)
+    i, j = 0, 0
+    while i < len(string):
+        if j == len(subString):
+            return i - j
+        elif string[i] == subString[j]:
+            i += 1
+            j += 1
+            continue
+        elif j > 0: 
+            j = next[j-1]
+            continue
+        elif j == 0: # 如果第一个匹配就不通过
+            i += 1  # 主指针向前
+            continue
+        
             
+        
+            
+    
