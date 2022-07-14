99ebef00b8c3d669b5ec1229c624f2eb98b59c7f
KSroido <ksxmyqj@gmail.com>
2022-07-14 14:37:31 +0800

    1

--- KMP.py
+++ KMP.py
@@ -1,6 +1,6 @@
 
 def buildNextArr(subString:str) -> list:
-    next = list(0)
+    next = [0]
     maxPrefixLenth = 0
     i = 1
     while i < len(subString):
@@ -21,20 +21,20 @@ def KMP(string:str,subString:str) -> int:
     next = buildNextArr(subString)
     i, j = 0, 0
     while i < len(string):
-        if j == len(subString):
-            return i - j
-        elif string[i] == subString[j]:
+        
+        if string[i] == subString[j]:
             i += 1
             j += 1
-            continue
         elif j > 0: 
             j = next[j-1]
-            continue
         elif j == 0: # 如果第一个匹配就不通过
             i += 1  # 主指针向前
-            continue
+        if j == len(subString):
+            return i - j
         
             
         
             
-    
+string = "AAABAAACAAA"
+subString = "AAACAAA"
+print(KMP(string, subString))
